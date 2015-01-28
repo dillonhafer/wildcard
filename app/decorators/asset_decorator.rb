dd7a93711fd50a722100a3d14bca61dba18ed4a8
Dillon Hafer <dh@dillonhafer.com>
2015-01-28 16:51:21 -0500

    Adds decorator methods for json

--- app/decorators/asset_decorator.rb
+++ app/decorators/asset_decorator.rb
@@ -1,9 +1,13 @@
 class AssetDecorator < LittleDecorator
   def name
-    if User.current_user.see_asset_id?
+    if current_user.see_asset_id?
       "#{model.name} (#{model.asset_id})"
     else
       model.name
     end
+  end
+
+  def as_json(options={})
+    {id: model.id, name: name}
   end
 end
