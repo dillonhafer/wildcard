a1387e027de5f988050b76ed0196ba887ea3f3e5
Dillon Hafer <dh@dillonhafer.com>
2015-01-29 13:51:27 -0500

    Simplify scope method

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,9 +1,6 @@
 class Asset < ActiveRecord::Base
   def self.search_in_fields(fields, word)
-    field_filter_sql = []
-    fields.each do |f|
-      field_filter_sql << "LOWER(#{f}) LIKE :word"
-    end
+    field_filter_sql = fields.map { |f| "LOWER(#{f}) LIKE :word" }
     where(field_filter_sql.join(' OR '), word: word)
   end
 end
